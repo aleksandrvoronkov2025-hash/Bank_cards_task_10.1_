4e97f2eb2d596ce3be9b6acc342db715be34d2d1
Voronkov A.V. <voronkov78@yandex.ru>
2025-10-28 20:19:48 +0400

    add src and tests

--- main.py
+++
@@ -1,7 +0,0 @@
-from src import masks
-
-mask_card_number = masks.get_mask_card_number("7000792289606361")
-print(mask_card_number)
-
-mask_account_number = masks.get_mask_account("73654108430135874305")
-print(mask_account_number)
