2f3b2efbf0f140cc170a2de537eda79f20f16ef6
Voronkov A.V. <voronkov78@yandex.ru>
2025-10-29 19:22:08 +0400

    Содержит пакет tests для тестирования 3-х модулей (с функциями): masks, processing и widget

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 
 from src.masks import get_mask_account, get_mask_card_number
 
+
 # 1. Фикстуры для тестирования функции get_mask_card_number:
 # 1.1 Проверка маскирования карты с корректным номером (из 16 цифр).
 @pytest.fixture
@@ -159,3 +160,119 @@ def get_mask_account_empty():
 @pytest.fixture
 def get_mask_account_0():
     return get_mask_account()  # 0 знаков
+
+
+# 3. Фикстуры тестирования функции mask_account_card модуля src.widget для:
+# 3.1 Проверки, что функция mask_account_card корректно распознает и применяет нужный тип
+# маскировки в зависимости от типа входных данных (карта или счет).
+@pytest.fixture
+def mask_card_visa_platinum():
+    return "Visa Platinum 7000792289606361"  # Карта Visa Platinum
+
+
+@pytest.fixture
+def mask_card_maestro():
+    return "Maestro 1234567890123456"  # Карта Maestro
+
+
+@pytest.fixture
+def mask_account():
+    return "Счет 73654108430135874305"  # Счет
+
+
+# 3.3 Тестирования функции mask_account_card на обработку некорректных входных данных
+# и проверка ее устойчивости к ошибкам.
+@pytest.fixture
+def mask_account_error_alpha_1():
+    return "Счет 7365410843013587430a"  # В счете последняя цифра заменена на букву
+
+
+@pytest.fixture
+def mask_account_error_alpha_2():
+    return "Сче  7365410843013587430a"  # В слове "счет" пропущена последняя буква, заменена последняя цифра на букву
+
+
+@pytest.fixture
+def mask_card_error_alpha():
+    return "aestro 7000792289606361"  # Недопустимое имя карты
+
+
+@pytest.fixture
+def mask_card_error_number():
+    return "Maestro 70007922896063611"  # Недопустимый номер карты
+
+
+@pytest.fixture
+def mask_card_error_number_alpha():
+    return "Maestr 70007922896063611"  # Недопустимые имя и номер карты
+
+
+# 5. Тестирование функции filter_by_state.
+# 5.1 Тестирование фильтрации списка словарей по заданному статусу state.
+results = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T081:33.419441"},
+]
+
+# 5.2 Проверка работы функции при отсутствии словарей с указанным статусом "state" в списке.
+results_1 = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "EXECUTED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "EXECUTED", "date": "2018-10-14T081:33.419441"},
+]
+
+
+@pytest.fixture
+def filter_by_state_results():
+    return results
+
+
+# 6. Фикстуры тестирования функции sort_by_date:
+
+# 6.1 Тестирование сортировки списка словарей по датам в порядке убывания и возрастания.
+
+
+@pytest.fixture
+def normal_different_date():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T081:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+# 6.2 Проверка корректности сортировки при одинаковых датах.
+results_of_states_same_date = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2019-07-03T18:36:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-09-12T01:33.419441"},
+]
+
+
+@pytest.fixture
+def same_date():
+    return results_of_states_same_date
+
+
+# 6.3 Тесты на работу функции с некорректными или нестандартными форматами дат.
+results_of_states_incorrect_date = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2024/03/11T18:35:29:512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "0001-04-31T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2025-02-29T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2020-04-31T081:33.419441"},
+    {"id": 594226725, "state": "CANCELED", "date": "2025-02-28T21:27:25.241689"},
+    {"id": 594226711, "state": "CANCELED", "date": "2024-02-30T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2020-04-31T081:33.419441"},
+    {"id": 594226725, "state": "CANCELED", "date": "2024-02-31T21:27:25.241689"},
+    {"id": 594226711, "state": "CANCELED", "date": "2025-02-31T21:27:25.241689"},
+]
+
+
+@pytest.fixture
+def incorrect_date():
+    return results_of_states_incorrect_date
